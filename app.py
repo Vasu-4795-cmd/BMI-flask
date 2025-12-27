f0dc8740842ac7f6ac9a5cffe6cb2014c17972b3
Vasu <vasuperugu6@gmail.com>
2025-12-27 10:56:46 +0530

    Update MySQL connection details

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ app.secret_key = "secret123"
 
 # MySQL connection
 db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="Deva@123",
-    database="bmi_app2"
+    host="mysql-2f5728bb-vasuperugu8-4ed2.i.aivencloud.com",
+    user="avnadmin",
+    password="AVNS_nXfPdOLnteKvZXs66sm",
+    database="bmi_app2",
+    port="21197"
 )
 cursor = db.cursor()
 
